48c0db0bc6379d0440f8c7a6e7246e46b83d0ac7
Andreia <andreia-mac@live.com>
2021-11-22 20:29:20 -0300

    Controller

--- routes/perfil.js
+++ routes/perfil.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const perfilController = require("../controllers/perfil")
 
+router.get ("/perfil", perfilController.index)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('perfil', { title: 'Users' });
 });
 
 module.exports = router;
+
--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,9 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('search', { title: 'Express' });
-// });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('search', { title: 'Express' });
+});
 
-// module.exports = router;
-
-const pageSearch = require('../Controllers/search');
-router.get('/', pageSearch.index)
+module.exports = router;
