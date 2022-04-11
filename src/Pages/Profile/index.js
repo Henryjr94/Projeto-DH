ca1d2ea5c20124f1a45a5f25126d1c0c9bbe9dcf
Deiiamac <andreia-mac@live.com>
2022-04-11 09:56:34 -0300

    update css perfil

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,150 +1,102 @@
-// import "./style.css"
+import "./style.css"
 import bookPlaceholder from "../../images/bookPlaceholder.jpg"
+import profile from "../../images/perfil.png"
 
 
 function Perfil() {
   return (
-    <main>
-      <section class="main">
+    <main className="profile">
+      <section>
         <h2>Meu Perfil</h2>
-        <section class="box">
-          <div class="foto_perfil">
-            <img src="/images/perfil.png" alt="Foto de Perfil" />
+        <section className="profile_box">
+          <div className="profile_foto">
+            <img src={profile} alt="Foto de Perfil" />
           </div>
-          <div class="info">
+          <div className="profile_info">
             <h3>João Silva</h3>
             <p><strong>E-mail: </strong>joao.silva@hotmail.com</p>
             <p><strong>Telefone </strong>11 94152-7895</p>
             <p><strong>Local: </strong> São Paulo</p>
-            <p><strong>Trocas realizadas: </strong>10</p>
-            <p><strong>Reputação: </strong>5 estrelas</p>
             <button>Editar</button>
-          </div>
-
-          <div class="info">
-            <p><strong>Trocas realizadas: </strong>10</p>
-            <p><strong>Reputação: </strong>5 estrelas</p>
           </div>
         </section>
 
         <h2>Livros para trocas</h2>
-        <div class="box_troca">
-          <div class="books">
-            <div class="book_box">
+        <div className="profile_box_troca">
+          <div className="profile_books">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
-            <div class="book_box">
+            <div className="profile_book_box">
               <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
-              <button>Trocar</button>
-            </div>
-          </div>
-        </div>
-
-        <h2>Livros que quero</h2>
-        <div class="box_troca">
-          <div class="books">
-            <div class="book_box">
-              <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
-              <button>Trocar</button>
-            </div>
-
-            <div class="book_box">
-              <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
-              <button>Trocar</button>
-            </div>
-            <div class="book_box">
-              <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
-              <button>Trocar</button>
-            </div>
-            <div class="book_box">
-              <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
-              <button>Trocar</button>
-            </div>
-            <div class="book_box">
-              <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
-              <button>Trocar</button>
-            </div>
-            <div class="book_box">
-              <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
-              <button>Trocar</button>
-            </div>
-            <div class="book_box">
-              <img src={bookPlaceholder} alt="Livro" />
-              <p>Coraline - Neil Gaiman</p>
+              <p>Book - author</p>
               <button>Trocar</button>
             </div>
           </div>
